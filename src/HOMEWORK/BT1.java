b97bad82e1a0d3e39ac0be049c094b5d64cdc17f
thd4t <103334099+thd4t@users.noreply.github.com>
2025-08-13 21:50:21 +0700

    First commit

---
+++ src/HOMEWORK/BT1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HOMEWORK;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class BT1 {
+    public static void main(String[] args){
+        int a = 1 + 2;
+        double b = 3.5 + 8;
+        int c = 1/2;
+        double d = 4.5+7;
+        int e = 4*5+1;
+        int g = 20%3+2-7;
+        
+        int x1 = 1;
+        int y = x1+ ++x1 - x1-- - x1++ + x1 - x1--;
+        int x2 = 1;
+        int z = x2 - x2-- - x2++ + --x2 -x2 + x2-- + x2;
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println("c = " + c);
+    System.out.println("d = " + d);
+    System.out.println("e = " + e);
+    System.out.println("g = " + g);
+    System.out.println(" ======= ");
+    System.out.println("x1 = " + x1);
+    System.out.println("y = " + y);
+    System.out.println("x2 = " + x2);
+    System.out.println("z = " + z);
+    } 
+    
+}
